e18ca7075202a83335b2d1ddc13a42ca4e8f78cd
hunghg255 <giahunghust@gmail.com>
2025-01-12 19:12:33 +0700

    chore: update

--- src/stylistic.js
+++ src/stylistic.js
@@ -11,12 +11,12 @@ export function stylistic() {
       rules: {
         'indent': ['error', 2],
         '@stylistic/indent': ['error', 2],
-        "@stylistic/semi-spacing": ["error", { "before": true, "after": false }],
+        "@stylistic/semi-spacing": ["error"],
         "@stylistic/no-multiple-empty-lines": ["error", { "max": 1, "maxBOF": 1 }],
         "@stylistic/function-call-argument-newline": ["error", "always"]
       }
     },
   ]
 
-  return config
+  return config ;
 }
